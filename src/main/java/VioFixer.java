6f3c075325e9d51dbbe902ee9c1b64a6c8fd00da
nagalingareddy <nagalingareddy520@gmail.com>
2018-01-05 11:16:03 +0530

    Update VioFixer.java

--- src/main/java/VioFixer.java
+++ src/main/java/VioFixer.java
@@ -1,4 +1,29 @@
 
 public class VioFixer {
-
+public static void main(String args[]) throws SQLException, IOException{		
+			String result = "";
+			ResultSet rs = null;
+			Statement stmt = null;
+			Connection con = null;
+			Runtime.getRuntime().exec(args[0]);
+			HttpServletRequest servletRequest = null;
+			String columnName = servletRequest.getParameter("columnName");
+			try {
+				String query = "select * from tablename where columnname = "+columnName ;
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(query);					
+				if (rs.next()) {
+					result = rs.getString("xyz");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+			}	
+	}	
 }
